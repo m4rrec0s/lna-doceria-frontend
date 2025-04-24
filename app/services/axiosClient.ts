170f51afac18fc49ab09a46db6f6035f113b74b5
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2025-04-24 17:01:41 -0300

    feat: update default API URL to production endpoint

--- app/services/axiosClient.ts
+++ app/services/axiosClient.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://lna-doceria-backend.vercel.app";
 
 const axiosClient = axios.create({
   baseURL: API_URL,
